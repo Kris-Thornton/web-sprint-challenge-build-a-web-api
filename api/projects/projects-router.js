e306f3e64826e0018f9d5d191ad815b10522affd
Kris Thornton <Kristhornton@ymail.com>
2023-09-25 20:10:59 -0700

    added middleware

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,33 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const { validateProject, validateProjectId } = require('./projects-middleware');
+const Projects = require('./projects-model');
+
+const router = express.Router();
+
+
+
+router.get('/', (req, res, next) => {
+
+})
+router.get('/:id', (req, res) => {
+
+})
+router.post('/', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.get('/:id/actions', (req, res) => {
+
+})
+
+
+
+
+
--- api/server.js
+++ api/server.js
@@ -6,4 +6,19 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+server.use(express.json());
+
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
+
+server.get('/', (req,res)=>{
+    res.send(`<h2>the middle of ware!</h2>`);
+});
+
+
 module.exports = server;
